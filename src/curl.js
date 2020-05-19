c67a0441ae9729936ff704bc34e931d63a9a97c1
Ariya Hidayat <ariya.hidayat@gmail.com>
2020-05-18 23:03:04 -0700

    Downloader using the external cURL

---
+++ src/curl.js
@@ -0,0 +1,28 @@
+const child_process = require('child_process');
+const fs = require('fs');
+
+const DEFAULT_TIMEOUT = 23; // seconds
+const MAX_SIZE = 4 * 1024 * 1024; // bytes
+
+function curl(url, fileName, timeout) {
+    timeout = timeout || DEFAULT_TIMEOUT;
+
+    const cmd = `curl -L -s -o ${fileName} -D - --max-time ${timeout} "${url}"`;
+
+    let content = null;
+    try {
+        child_process.execSync(cmd).toString();
+        const responseSize = fs.statSync(fileName).size;
+        if (responseSize < MAX_SIZE) {
+            content = fs.readFileSync(fileName, 'utf-8').toString();
+        } else {
+            console.error(`Download is too big: ${responseSize} (exceeding ${MAX_SIZE / 1024 / 1024} MB)`);
+        }
+    } catch (err) {
+        // Usually indicate a timeout situation
+        console.error(`Failed cmd: ${err.message}`);
+    }
+    return content;
+}
+
+module.exports = curl;
