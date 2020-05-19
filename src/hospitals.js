62b87c2d2736d5a1b9c7de9efb85a5a07dec22fc
Ariya Hidayat <ariya.hidayat@gmail.com>
2020-05-19 04:58:56 -0700

    hospitals data: trim strings

--- src/hospitals.js
+++ src/hospitals.js
@@ -15,8 +15,13 @@ function updateCovid19Hospitals() {
     const jqScript = 'src/hospitals.jq';
 
     child_process.execSync(`cat ${rawName} | jq -f ${jqScript} > ${fileName}`);
-    const hospitals = JSON.parse(fs.readFileSync(fileName, 'utf-8').toString());
-    console.log(`Found ${hospitals.length} hospitals`);
+    const rawHospitalList = JSON.parse(fs.readFileSync(fileName, 'utf-8').toString());
+    console.log(`Found ${rawHospitalList.length} hospitals`);
+    const hospitals = rawHospitalList.map((h) => {
+        let hh = {};
+        Object.keys(h).forEach((k) => (hh[k] = typeof h[k] === 'string' ? h[k].trim() : h[k]));
+        return hh;
+    });
 
     fs.writeFileSync('public/api/id/covid19/hospitals', JSON.stringify(hospitals, null, 2));
 
