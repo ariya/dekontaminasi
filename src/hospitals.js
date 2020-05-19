069792620fd94719b2b8f051700c1b9f2ba4d5bf
Ariya Hidayat <ariya.hidayat@gmail.com>
2020-05-19 04:38:02 -0700

    Digest the hospital data

---
+++ src/hospitals.js
@@ -0,0 +1,26 @@
+const child_process = require('child_process');
+const fs = require('fs');
+
+const mkdirp = require('./mkdirp');
+
+function updateCovid19Hospitals() {
+    console.log('Updating Indonesia COVID-19 Hospitals...');
+
+    mkdirp('public/api');
+    mkdirp('public/api/id');
+    mkdirp('public/api/id/covid19');
+
+    const fileName = 'public/api/id/covid19/hospitals.json';
+    const rawName = 'public/api/cache/RS_Rujukan_COVID19_Indonesia';
+    const jqScript = 'src/hospitals.jq';
+
+    child_process.execSync(`cat ${rawName} | jq -f ${jqScript} > ${fileName}`);
+    const hospitals = JSON.parse(fs.readFileSync(fileName, 'utf-8').toString());
+    console.log(`Found ${hospitals.length} hospitals`);
+
+    fs.writeFileSync('public/api/id/covid19/hospitals', JSON.stringify(hospitals, null, 2));
+
+    console.log('COMPLETED.');
+}
+
+updateCovid19Hospitals();
