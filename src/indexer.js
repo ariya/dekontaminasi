b41c40db1f6035869ce6a8e1a00d058cc0bfbd94
Ariya Hidayat <ariya.hidayat@gmail.com>
2020-05-24 00:51:29 -0700

    File indexer for index.html

---
+++ src/indexer.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function removeFile(fname) {
+    if (fs.existsSync(fname)) fs.unlinkSync(fname);
+}
+
+function findFiles(dir) {
+    const files = [];
+    fs.readdirSync(dir).forEach((file) => {
+        const name = dir + '/' + file;
+        if (fs.statSync(name).isDirectory()) files.push(...findFiles(name));
+        else files.push(name);
+    });
+    return files;
+}
+
+function indexFiles() {
+    console.log('Creating content index for index.html...');
+
+    // Raw JSONs shouldn't be served
+    removeFile('public/api/id/covid19/national.json');
+    removeFile('public/api/id/covid19/provinces.json');
+    removeFile('public/api/id/covid19/hospitals.json');
+
+    const content = findFiles('public/api/id')
+        .map((fname) => {
+            const path = fname.replace('public/', '');
+            return `<p><a href="${path}">${path}</a></p>`;
+        })
+        .join('\n');
+
+    const index = fs.readFileSync('public/index.html', 'utf-8').toString();
+    const modifiedIndex = index.replace('<!--index-->', content);
+    fs.writeFileSync('public/index.html', modifiedIndex);
+
+    console.log('COMPLETED.');
+}
+
+indexFiles();
