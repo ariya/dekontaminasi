b734fe20357d43c423ee3853f34d6200dcff2413
Zain Fathoni <zain.fathoni@gmail.com>
2020-07-03 14:07:28 +0800

    Update ping script to avoid overriding the existing _headers content

--- src/ping.js
+++ src/ping.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const now = Date.now();
 console.log(`Now is ${new Date(now).toGMTString()}`);
 
-const headerConfig = ['/ping', ' X-Update-Timestamp: ' + now.toString()].join('\n');
-fs.writeFileSync('public/_headers', headerConfig);
+const headers = fs.readFileSync('public/_headers', 'utf-8').toString();
+const pingHeaders = ['/ping', '  X-Update-Timestamp: ' + now.toString()].join('\n');
+const modifiedHeaders = headers.replace('#ping-placeholder', pingHeaders);
+fs.writeFileSync('public/_headers', modifiedHeaders);
 fs.writeFileSync('public/ping', 'OK');
